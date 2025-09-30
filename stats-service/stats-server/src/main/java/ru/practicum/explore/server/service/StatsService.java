8ead955896f6bf674a4fe6ff8d2046981761c590
Vlasislav <vladsbp80@yandex.com>
2025-09-30 04:56:13 +0300

    feat: add stats-service (исправления Checkstyle)

--- stats-service/stats-server/src/main/java/ru/practicum/explore/server/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/explore/server/service/StatsService.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsService {
+
     void saveHit(EndpointHitDto endpointHit);
+
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
